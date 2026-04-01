53974c04ea68c3cc500cc3fc6cb56ff7c3d56b2f
agarcia123 <garciaaugusto@gmail.com>
2026-04-01 13:41:48 -0400

    added 2 more test, added a class fro score

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -13,3 +13,26 @@ test('has a greeting section', async ({ page }) => {
   await expect(page.locator('[data-testid="greeting"]'))
   .toContainText('Welcome to the Mr President challenge, you will be given 5 questions about current events and you will have to choose the best answer, good luck!');
 });
+
+test('has a question title', async ({ page }) => {
+  await page.goto('http://localhost:3000/');   
+  // Expects page to have a question title
+  await expect(page.locator('[data-testid="question-title"]'))
+  .toContainText('If you were president how would you handle this situation');
+});
+
+test('has 3 options', async ({ page }) => {
+  await page.goto('http://localhost:3000/');   
+  // Expects page to have 3 options
+  await expect(page.locator('[data-testid="option-0-radiobutton"]')).toBeVisible();
+  await expect(page.locator('[data-testid="option-1-radiobutton"]')).toBeVisible();
+  await expect(page.locator('[data-testid="option-2-radiobutton"]')).toBeVisible();
+});
+
+test('score updates when an option is selected', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  // Click the first option
+  await page.click('[data-testid="option-0-radiobutton"]');
+  // Expects score to be updated
+  await expect(page.locator('[data-testid="score"]')).not.toContainText('Your score is: 0');
+});
